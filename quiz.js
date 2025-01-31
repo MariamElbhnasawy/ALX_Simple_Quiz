0f2bc2f8cea3580a355f717825144f5a96576be9
MariamElbhnasawy <mariam.elbahey.hammam@gmail.com>
2025-01-31 17:26:28 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 function checkAnswer() {
-    let correctAnswer = document.getElementById("choice1");
+    let correctAnswer = "4";
 
     let userAnswer = document.querySelector('input[name="quiz"]:checked');
 
@@ -8,7 +8,7 @@ function checkAnswer() {
         return;
     }
 
-    if (userAnswer === correctAnswer) {
+    if (userAnswer.value === correctAnswer) {
         document.getElementById("feedback").textContent = "Correct! Well done.";
     } else {
         document.getElementById("feedback").textContent = "That's incorrect. Try again!";
